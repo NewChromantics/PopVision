b286d1e3b93b6301bf409c97d8f5f7791dd2d72a
Graham Reeves <graham@grahamreeves.com>
2019-11-19 17:33:21 +0000

    Added ios target

---
+++ PopCoreml_Ios/PopCoreml_Ios.h
@@ -0,0 +1,19 @@
+//
+//  PopCoreml_Ios.h
+//  PopCoreml_Ios
+//
+//  Created by graham on 19/11/2019.
+//  Copyright © 2019 NewChromantics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopCoreml_Ios.
+FOUNDATION_EXPORT double PopCoreml_IosVersionNumber;
+
+//! Project version string for PopCoreml_Ios.
+FOUNDATION_EXPORT const unsigned char PopCoreml_IosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopCoreml_Ios/PublicHeader.h>
+
+
