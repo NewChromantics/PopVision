77cb2cb5616208414b867605def4cfab42c02540
Graham Reeves <graham@grahamreeves.com>
2020-07-23 23:57:56 +0100

    Renaming project to PopVision

---
+++ PopVision.Android/obj/local/armeabi-v7a/objs/PopMl/__/__/Source/SoyLib/src/SoyDebug.o.d
@@ -0,0 +1,113 @@
+/Volumes/Code/PopCoreml/PopMl.Android//obj/local/armeabi-v7a/objs/PopMl/__/__/Source/SoyLib/src/SoyDebug.o: \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyDebug.cpp \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyDebug.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes_Android.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/memory \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTime.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/iomanip \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyMath.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyVector.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/utilities.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_2.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_3.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_4.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix_4x4.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/RemoteArray.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyArray.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyAssert.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/chrono \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/thread \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyScope.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyString.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/MemHeap.hpp \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/Array.hpp \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/mutex \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/BufferArray.hpp \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/queue \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyThread.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/condition_variable \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/HeapArray.hpp \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cassert \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyDebug.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes_Android.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/memory:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTime.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/iomanip:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyMath.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyVector.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/utilities.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_2.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_3.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_4.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix_4x4.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/RemoteArray.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyArray.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyAssert.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/chrono:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/thread:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyScope.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyString.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/MemHeap.hpp:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/Array.hpp:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/mutex:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/BufferArray.hpp:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/queue:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyThread.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/condition_variable:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/HeapArray.hpp:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cassert:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
---
+++ PopVision.Android/obj/local/x86_64/objs/PopMl/__/__/Source/TCoreMl.o.d
@@ -0,0 +1,110 @@
+/Volumes/Code/PopCoreml/PopMl.Android//obj/local/x86_64/objs/PopMl/__/__/Source/TCoreMl.o: \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/TCoreMl.cpp \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/TCoreMl.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/PopCoreml.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/Array.hpp \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/mutex \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes_Android.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/memory \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/RemoteArray.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyArray.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyAssert.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyVector.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/utilities.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_2.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_3.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_4.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix_4x4.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/HeapArray.hpp \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cassert \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/MemHeap.hpp \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/BufferArray.hpp \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/queue \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTime.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/iomanip \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyMath.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyDebug.h \
+  /usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/thread \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyScope.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyString.h \
+  /Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/TTestSkeleton.h
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/TCoreMl.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/PopCoreml.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/Array.hpp:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/mutex:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTypes_Android.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/memory:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/RemoteArray.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyArray.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyAssert.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyVector.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/utilities.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_2.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_3.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/vector_4.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix_4x4.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/mathfu/matrix.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/HeapArray.hpp:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cassert:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/MemHeap.hpp:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/BufferArray.hpp:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/queue:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyTime.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/iomanip:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyMath.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyDebug.h:
+
+/usr/local/Cellar/android-ndk/r13b/sources/cxx-stl/gnu-libstdc++/4.9/include/thread:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyScope.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/SoyLib/src/SoyString.h:
+
+/Volumes/Code/PopCoreml/PopMl.Android//jni/../../Source/TTestSkeleton.h:
